ba1636920c15c42b0256db218071183c8d6a50af
Thiago Holanda <thimholanda@gmail.com>
2023-09-10 02:35:36 -0300

    fix(features):  animation adjustment

--- src/actions.js
+++ src/actions.js
@@ -77,7 +77,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function resetAccordion() {
     accordionItems.forEach((item, index) => {
-      item.querySelector(".features-accordion__content").style.display = "none";
       item.querySelector(".features-accordion__content").classList.remove("features-accordion__content--height-auto");
       item.querySelector(".features-accordion__progress-bar").style.display = "none";
       item.querySelector(".features-accordion__progress").style.width = "0%";
@@ -93,6 +92,7 @@ document.addEventListener("DOMContentLoaded", () => {
           imgsAccordion[index].style.transform = "scale(1)";
         }, 400);
         item.querySelector(".features-accordion__content").style.display = "";
+        item.querySelector(".features-accordion__content").style.height = "auto";
         setTimeout(() => item.querySelector(".features-accordion__content").classList.add("features-accordion__content--height-auto"), 10);
 
         item.querySelector(".features-accordion__progress-bar").style.display = "";
